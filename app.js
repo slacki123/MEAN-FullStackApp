5668279fc4f216a1a50d8758b3207384aa195b50
unknown <sebastian.lacki@academytrainee.com>
2018-12-09 22:49:26 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require("./instantHello");
+var goodbye1 = require("./talk/goodbye");
+var talk1 = require("./talk");
+
+//var needs () added to the variable name clearly..
+//"talk" is the variable name specified above
+talk1.intro();
+talk1.hello("seb");
+
+
+goodbye1();
+
